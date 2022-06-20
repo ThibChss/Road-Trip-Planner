cb88a15aa20c6f0fe21745d68453eb75c3b1d598
Thibault Chassine <thibault.chassine@gmail.com>
2022-06-20 21:04:29 +0200

    fixed models validations and columns names

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,17 +2,12 @@ class Trip < ApplicationRecord
   STATUS = %w[cancelled confirmed pending]
 
   belongs_to :user
-  has_many :activities, :transportations, :accomodations, dependent: :destroy
+  has_many :activities, dependent: :destroy
+  has_many :transportations, dependent: :destroy
+  has_many :accomodations, dependent: :destroy
   validates :name, :start_date, :end_date, :status, presence: true
   validates :status, acceptance: { accept: STATUS }
-  validate :end_date_after_start_date?
-
-  private
-
-  def end_date_after_start_date?
-    # Confirm that the end date cannot be before the start date
-    if :end_date < :start_date
-      errors.add :end_date, "Must be after the starting date"
-    end
-  end
 end
+
+# Test
+# trip = Trip.create(name: 'El Triposs', start_date: Date.today - 1, end_date: Date.today + 10, status: 'confirmed', user: user)
